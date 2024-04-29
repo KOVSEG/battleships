d199ea23f1566f5a3b8d4a60eafbeff159664d5a
Kovseg <vladislavkoritskiy@yandex.ru>
2024-04-29 17:47:52 +0300

    clear form after every click of Fire button, shows hits and misses on the table

--- script.js
+++ script.js
@@ -6,20 +6,27 @@
 
 let model = {
   ships: [
-    { shipLength: 4, coordinates: [01, 02, 03, 04], hits: ['', '', '', ''] },
+    { shipLength: 4, localisation: ['01', '02', '03', '04'], hits: [] },
   ],
   fire: function (coordinates) {
-
+    for(ship of this.ships) {
+      if(ship.localisation.includes(coordinates)) {
+        ship.hits.push('hit');
+        view.hit(coordinates);
+      } else {
+        view.miss(coordinates)
+      }
+    }
   }
 };
 
 let view = {
-  miss: function (coordinates) {
+  miss: function(coordinates) {
     let showMiss = document.getElementById(coordinates);
     showMiss.classList.add('miss');
     showMiss.innerText = 'MISS';
   },
-  hit: function (coordinates) {
+  hit: function(coordinates) {
     let showMiss = document.getElementById(coordinates);
     showMiss.classList.add('hit');
     showMiss.innerText = 'HIT';
@@ -27,23 +34,34 @@ let view = {
 };
 
 let controller = {
-  getCoords: function () {
+  getCoords: function(param) {
+    let coordSymb = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
+    let getParam = '';
 
-  }
+    if(param.length > 2) {
+      getParam = coordSymb.indexOf(param[0]) + param[1] + param[2];
+    } else {
+      getParam = coordSymb.indexOf(param[0]) + param[1];
+    }
+
+    // далее необходимо вызвать модель, обработать введеные координаты, определить попадание было или промах и после полученного итога вывести на игровом поле
+    model.fire(getParam);
+  },
 };
+
 
 function checkCoordinates() {
   let coordinates = document.getElementById('coordinates');
   let coordSign = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J'];
 
-  let coordinatesValue = coordinates.value;
-  let firstCharCoords = coordinatesValue.charAt(0).toUpperCase();
+  let coordinatesValue = coordinates.value.toUpperCase();
+  let firstCharCoords = coordinatesValue.charAt(0);
 
   if(!coordinatesValue.includes(' ')) {
     //Checking whitespace in the coordinates
 
     if(coordinatesValue.length < 1) {
-      console.log('Enter the coordinates');
+      alert('Enter the coordinates');
     } else {
 
       let checkLetter = new RegExp('^[a-zA-Z]+$');
@@ -54,37 +72,42 @@ function checkCoordinates() {
         if(checkSecondNum.test(coordinatesValue.charAt(1))) {
           //Checking the second character, it must be a digit
           if(coordinatesValue.length === 2) {
+            controller.getCoords(coordinatesValue);
             console.log(coordinatesValue);
           } else {
 
             if(coordinatesValue.length === 3) {
               let checkThirdNum = new RegExp('^[0]+$');
               if(checkSecondNum.test(coordinatesValue.charAt(1)) && checkLetter.test(coordinatesValue.charAt(2))) {
-                console.log('Don\'t write letters after the second coordinates, it will come to mistake');
+                alert('Don\'t write letters after the second coordinates, it will come to mistake');
 
               } else if(coordinatesValue.charAt(1) == 1 && checkThirdNum.test(coordinatesValue.charAt(2))) {
                 //Checking the second and the third characters - both digits. The second character is equal the one, the third is equal to zero
-                console.log(coordinatesValue);
+                controller.getCoords(coordinatesValue);
               } else {
-                console.log('The maximum of digit\'s coordinate value at the battlefield is only 10');
+                alert('The maximum of digit\'s coordinate value at the battlefield is only 10');
               }
             } else {
-              console.log('You wrote too much characters, please write the coordinates correctly, for example: A5, b10, J9, c3. The register doesn\'t matter');
+              alert('You wrote too much characters, please write the coordinates correctly, for example: A5, b10, J9, c3. The register doesn\'t matter');
             }
 
           }
         } else {
-          console.log('The second coordinate must be equal with digits above the battlefield');
+          alert('The second coordinate must be equal with digits above the battlefield');
         }
 
       } else {
-        console.log('The first coordinate must be equal with values on the left side of the battlefield');
+        alert('The first coordinate must be equal with values on the left side of the battlefield');
       }
 
     }
   } else {
-    console.log('Don\'t use whitespace in the coordinates');
+    alert('Don\'t use whitespace in the coordinates');
   }
+
+  coordinates.value = '';
+  return null;
+
 }; //checkCoordinates function
 
 
